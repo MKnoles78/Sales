c99e328752e1608d9a519f60ca8b14cc1cececca
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 17:34:17 -0500

    added in initial working code

--- src/Sales.java
+++ src/Sales.java
@@ -1,9 +1,41 @@
 
+//Author Mike Knoles PA 3.8
+//Exercise 7.10 Solution: Sales.java
+//number of salespeople that earned salaries in given ranges
+import java.util.Scanner;
+
 public class Sales {
 
+	// counts the number of people in given salary ranges
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		int[] total = new int[9]; //totals for the various salaries
+		
+		//initialize the values int the array to 0
+		
+		//read in values and assign them to the appropriate range
+		System.out.print("Enter sales amount (negative to end): ");
+		double dollars = input.nextDouble();
+		
+		while (dollars >= 0) {
+			double salary = dollars * 0.09 + 200;
+			int range = (int)(salary / 100);
+			
+			if(range > 10)
+				range = 10;
+			++total[range -2];
+			
+			System.out.print("Enter sales amount (negative to end): ");
+			dollars = input.nextDouble();
+		}
+		
+		//print chart - This can be one line of code
+		
+		for () 
+			System.out.printf("$%d$%d\t%d\n", (200 + 100 * range), (299 + 100 * range), total[range]);
+		
+		//special case for the last range
+		System.out.printf("$1000 and over\t%d\n", total[total.length - 1]);
 	}
-
-}
+} // end class Sales
